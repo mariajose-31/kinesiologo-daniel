820f4cde9019844f853a7d81d1105d309a2a0783
mariajose-31 <mariajoseperezrengifo@gmail.com>
2025-10-06 17:04:07 -0500

    Delete Kinesologo directory

--- Kinesologo/script.js
+++
@@ -1,6 +0,0 @@
-// Rellenar año y pequeñas interacciones si se desean luego
-document.addEventListener('DOMContentLoaded', () => {
-  const y = new Date().getFullYear();
-  const el = document.getElementById('year');
-  if (el) el.textContent = y;
-});
